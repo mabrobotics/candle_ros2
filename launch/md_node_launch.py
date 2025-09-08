b00127550b930e083e462af5a03c682c80193985
Krzysztof Kwapisz <krzysiek.kwapisz@gmail.com>
2025-09-08 17:07:02 +0200

    feat: launch selected nodes

---
+++ launch/md_node_launch.py
@@ -0,0 +1,24 @@
+# md_node_launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="candle_ros2",
+                executable="candle_container",
+                name="candle_container",
+                output="screen",
+                parameters=[
+                    {
+                        "launch_md_node": True,
+                        "launch_pds_node": False,
+                        "data_rate": "1M",
+                        "bus": "USB",
+                    }
+                ],
+            )
+        ]
+    )
--- src/candle_container.cpp
+++ src/candle_container.cpp
@@ -1,6 +1,4 @@
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp_components/component_manager.hpp"
-
 #include "candle_ros2/utils/candle_factory.hpp"
 #include "candle_ros2/utils/candle_params.hpp"
 #include "candle_ros2/md_node.hpp"
@@ -10,17 +8,31 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
-    auto           config_node = std::make_shared<rclcpp::Node>("candle_config");
-    candleParams_S params      = readParams(config_node);
-    auto           candle      = createCandle(params);
+    auto config_node = std::make_shared<rclcpp::Node>("candle_config");
 
-    auto md_node  = std::make_shared<MdNode>(rclcpp::NodeOptions(), candle, params);
-    auto pds_node = std::make_shared<PdsNode>(rclcpp::NodeOptions(), candle, params);
+    config_node->declare_parameter<bool>("launch_md_node", true);
+    config_node->declare_parameter<bool>("launch_pds_node", true);
+
+    bool launch_md  = config_node->get_parameter("launch_md_node").as_bool();
+    bool launch_pds = config_node->get_parameter("launch_pds_node").as_bool();
+
+    candleParams_S params = readParams(config_node);
+    auto           candle = createCandle(params);
 
     rclcpp::executors::MultiThreadedExecutor exec;
-    exec.add_node(md_node);
-    exec.add_node(pds_node);
     exec.add_node(config_node);
+
+    if (launch_md)
+    {
+        auto md_node = std::make_shared<MdNode>(rclcpp::NodeOptions(), candle, params);
+        exec.add_node(md_node);
+    }
+
+    if (launch_pds)
+    {
+        auto pds_node = std::make_shared<PdsNode>(rclcpp::NodeOptions(), candle, params);
+        exec.add_node(pds_node);
+    }
 
     exec.spin();
     rclcpp::shutdown();

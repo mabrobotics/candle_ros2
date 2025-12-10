abb3d530b905ff82d8bc6754d82a365c8eb80e1b
Krzysztof Kwapisz <krzysiek.kwapisz@gmail.com>
2025-12-10 10:06:54 +0100

    feat: change launch parameters from command line

--- launch/md_node_launch.py
+++ launch/md_node_launch.py
@@ -1,11 +1,30 @@
 # md_node_launch.py
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    bus_arg = DeclareLaunchArgument(
+        "bus",
+        default_value="USB",
+        description="Bus type: USB or SPI",
+    )
+
+    data_rate_arg = DeclareLaunchArgument(
+        "data_rate",
+        default_value="1M",
+        description="Data rate: 1M, 2M, 5M or 8M",
+    )
+
+    bus = LaunchConfiguration("bus")
+    data_rate = LaunchConfiguration("data_rate")
+
     return LaunchDescription(
         [
+            bus_arg,
+            data_rate_arg,
             Node(
                 package="candle_ros2",
                 executable="candle_container",
@@ -14,10 +33,10 @@ def generate_launch_description():
                     {
                         "launch_md_node": True,
                         "launch_pds_node": False,
-                        "data_rate": "1M",
-                        "bus": "USB",
+                        "data_rate": data_rate,
+                        "bus": bus,
                     }
                 ],
-            )
+            ),
         ]
     )
